4a170054ca495bf514b452693b9eb6b9258f2ce8
Daniel Sportes <daniel@sportes.fr>
2023-08-21 16:12:36 +0200

    Ajustements

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,14 @@
-const path = require('path')
-// const WebpackObfuscator = require('webpack-obfuscator');
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
 
-module.exports = {
-  entry: './src/server.mjs',
+const path = require('path')
+
+export default {
+  entry: './src/server.js',
   target: 'node',
   mode: 'production', // 'development' ou 'production'
   output: {
     filename: 'app.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve('dist'),
   }
-  /*
-  ,
-  // webpack plugins array
-  plugins: [
-    new WebpackObfuscator ({
-      rotateStringArray: true
-    } //, ['excluded_bundle_name.js']
-    )
-  ]
-  */
 }
