63f6a17ed8ee15c5b54d7da96f15787196c78e1c
Daniel Sportes <daniel@sportes.fr>
2024-08-29 12:31:04 +0200

    Détail

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,19 +1,22 @@
-// const path = require('path')
-// module.exports = {
 import path from 'path'
 export default {
-  entry: './src/server.js',
+  entry: './src/server.js', // pubsub.js
   target: 'node',
   mode: 'production', // 'development' ou 'production'
   output: {
-    filename: 'app.js',
-    path: path.resolve('dist'),
-  },
-  externals: {
-    // 'better-sqlite3': 'commonjs better-sqlite3'
-    /* Avec cette directive, app.js N'EST PLUS AUTONOME
-    Il lui faut node_modules (au moins une partie).
-    DONC IL FAUT obtenir better-sqlite3 par require()
-    */
+    filename: 'srv.js', // pubsub.js
+    path: path.resolve('dist/srv') // dist/pubsub
   }
 }
+
+/*
+  Avec cette directive (sous output), on pourrait faire un 
+  import de better-sqlite3 plutôt qu'un require
+  MAIS alors app.js N'EST PLUS AUTONOME
+  Il lui faut node_modules (au moins une partie).
+  DONC IL FAUT obtenir better-sqlite3 par require()
+
+  externals: { 'better-sqlite3': 'commonjs better-sqlite3' }
+
+*/
+
